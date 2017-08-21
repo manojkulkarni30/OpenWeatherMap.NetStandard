80114e855eac0b4e6b15a81a2eed4f022a67532c
Manoj Kulkarni <manoj.kulkarni.3011@gmail.com>
2017-08-21 20:20:04 +0530

    Sample project is added

---
+++ sample/OpenWeatherMap.NetStandard.Sample/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using OpenWeatherMap.NetStandard.Models;
+using OpenWeatherMap.NetStandard.Sample.Models;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace OpenWeatherMap.NetStandard.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<IActionResult> Index(string cityName="")
+        {
+            var homeViewModel = new HomeViewModel();
+            homeViewModel.CityName = cityName;
+            if(!string.IsNullOrWhiteSpace(homeViewModel.CityName))
+            {
+                var openWeatherMapApiKey = _configuration["OpenWeatherMapApiKey"];
+                var openWeatherMapClient = new OpenWeatherMapClient(openWeatherMapApiKey);
+                homeViewModel.WeatherApiResponse = await openWeatherMapClient
+                    .GetWeatherByCityNameAsync(cityName,
+                    unit:Unit.Metric);
+            }
+            return View(homeViewModel);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ sample/OpenWeatherMap.NetStandard.Sample/Models/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using OpenWeatherMap.NetStandard.Models;
+
+namespace OpenWeatherMap.NetStandard.Sample.Models
+{
+    public class HomeViewModel
+    {
+        public string CityName { get; set; }
+
+        public WeatherApiResponse WeatherApiResponse { get; set; }
+    }
+}
